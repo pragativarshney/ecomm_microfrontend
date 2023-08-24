83d3c1753f7b5639e20098f4c056af8013d7d0c0
Pragati Varshney <pragavarshney@deloitte.com>
2023-08-24 19:28:05 +0530

    faker dependency update

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,10 @@
-import faker,{commerce} from 'faker';
-import commerce from
+import {faker} from '@faker-js/faker';
 
 let products='';
 
 for(let i=0;i<4;i++){
-    const name= faker.commerce;
+    const name= faker.commerce.productName();
     products += `<div>${name}</div>`
 }
-console.log(products);
+console.log(products)
+document.querySelector("#dev-products").innerHTML = products;
